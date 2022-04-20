6195d56f5cfa072985b226302816bee89e580adf
Carlos Escobar <skalero01@gmail.com>
2022-04-20 00:08:34 -0500

    vistabella

---
+++ vistabella/folder/file.js
@@ -0,0 +1,12 @@
+file({
+    Image: {
+        xmlns: 'http://schemas.microsoft.com/deepzoom/2008',
+        Format: 'jpg',
+        Overlap: 1,
+        TileSize: 256,
+        Size: {
+            Width: 18406,
+            Height: 16887
+        }
+    }
+});
